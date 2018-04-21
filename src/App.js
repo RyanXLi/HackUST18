2b71c1ec16c8607aa2f055a61e81bd4e4bd8ef1f
LAPTOP-0BM0R1NJ\BaiJiaxin <jbai0318@gmail.com>
2018-04-21 23:44:36 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import AV from 'leancloud-storage';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor(props) {
+    const currentUser = AV.User.current();
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  Log() {
+    console.log("hahah");
+  }
+/*
+  renderLogin() {
+    if (this.state.isLoggedIn) {
+      return <div>{currentUser.getUsername()}</div>
+    }
+    else {
+      return(
+        <div>
+          <ModalBasicExample loginCallback={()=>this.handleLoginClick()}/>
+        </div>
+      )
+    }
+  }
+
+*/
   render() {
     return (
       <div className="App">
@@ -12,6 +43,7 @@ class App extends Component {
             <header className="App-header">
               {/*<img src={logo} className="App-logo" alt="logo" />*/}
               <h1 className="App-title">Welcome to React</h1>
+              <ModalBasicExample loginCallback={() => this.Log()}/>
               <div className="Nav">
                 <Link className="Nav-item" to="/">MainPage</Link>
                 <Link className="Nav-item" to="/catalog">CatalogPage</Link>
@@ -96,6 +128,24 @@ const Topic = ({ match }) => (
     <h3>{match.params.topicId}</h3>
   </div>
 );
+
+
+const ModalBasicExample = (loginCallback) => (
+  <Modal trigger={<Button>Basic Modal</Button>} basic size='small'>
+    <Header icon='archive' content='Archive Old Messages' />
+    <Modal.Content>
+      <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button basic color='red' inverted onClick={() => loginCallback()}>
+        <Icon name='remove' /> No
+      </Button>
+      <Button color='green' inverted onClick={() => loginCallback()}>
+        <Icon name='checkmark' /> Yes
+      </Button>
+    </Modal.Actions>
+  </Modal>
+)
 
 
 export default App;
