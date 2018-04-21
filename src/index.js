f33bbf78cfd3ef5c96bc050a4f464074861f2764
LAPTOP-0BM0R1NJ\BaiJiaxin <jbai0318@gmail.com>
2018-04-21 16:46:08 +0800

    hello

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import AV from 'leancloud-storage';
+
+AV.init({
+      appId: '1k0VJ8utD1ElnG8ecxnORrcK-gzGzoHsz',
+      appKey: '9BnyPpD91ixcpYzCbWNt55FU'
+    });
+
+    // 使用 JavaScript 的 API 查询云存储中的数据。
+    new AV.Query('Todo').find().then(function(todos) {
+      console.log(todos);
+      console.log("leancloud Ready")
+    }).catch(function(err) {
+      console.log(err)
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
